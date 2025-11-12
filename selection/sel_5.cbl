68429ef8db16a3302aa6d583fa933e87faef0bd6
Sanchez <gabriel.raknchez@gmail.com>
2025-11-12 23:42:25 +0800

    sel 5 complete tests done with bug fixes

--- selection/sel_5.cbl
+++ selection/sel_5.cbl
@@ -2,14 +2,33 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. SMALLEST-OF-THREE.
 
+       ENVIRONMENT DIVISION.
+           INPUT-OUTPUT SECTION. 
+               FILE-CONTROL.
+           SELECT SEL5-FILE ASSIGN TO "sel_5.dat"
+-              ORGANIZATION IS LINE SEQUENTIAL. 
+
        DATA DIVISION.
+       FILE SECTION.
+           FD SEL5-FILE.
+
+       01  SEL5_RECORD.
+           05  SEL5_F_OUTPUT_FH            PIC ZZ,ZZZ.
+           05  SEL5_S_OUTPUT_FH            PIC ZZ,ZZZ.
+           05  SEL5_T_OUTPUT_FH            PIC ZZ,ZZZ.
+           05  SEL5_RESULT_FH              PIC ZZ,ZZZ.
+           
+           
        WORKING-STORAGE SECTION.
-       01  SEL5_F_INPUT             PIC 9(5).
-       01  SEL5_F_OUTPUT            PIC Z(5).
-       01  SEL5_S_INPUT             PIC 9(5).
-       01  SEL5_S_OUTPUT            PIC Z(5).
-       01  SEL5_T_INPUT             PIC 9(5).
-       01  SEL5_T_OUTPUT            PIC Z(5).
+       01  SEL5_F_INPUT                PIC 9(5).
+       01  SEL5_F_OUTPUT               PIC Z(5).
+       01  SEL5_S_INPUT                PIC 9(5).
+       01  SEL5_S_OUTPUT               PIC Z(5).
+       01  SEL5_T_INPUT                PIC 9(5).
+       01  SEL5_T_OUTPUT               PIC Z(5).
+
+       01  SEL5_EOF                    PIC X VALUE "N".
+       01  SEL5_HISTORY                PIC X VALUE "N".
       
 
        PROCEDURE DIVISION.
@@ -27,19 +46,56 @@
 -              WITH NO ADVANCING.
            ACCEPT SEL5_T_INPUT.
            MOVE SEL5_T_INPUT TO SEL5_T_OUTPUT.
+
+           OPEN EXTEND SEL5-FILE.
+               MOVE SEL5_F_INPUT TO SEL5_F_OUTPUT_FH.
+               MOVE SEL5_S_INPUT TO SEL5_S_OUTPUT_FH.
+               MOVE SEL5_T_INPUT TO SEL5_T_OUTPUT_FH.
            
            EVALUATE TRUE
 
            WHEN SEL5_F_INPUT <= SEL5_S_INPUT 
 -              AND SEL5_F_INPUT <= SEL5_T_INPUT
                    DISPLAY SEL5_F_OUTPUT " IS THE LOWEST NUMBER"
+                   MOVE SEL5_F_OUTPUT_FH TO SEL5_RESULT_FH
 
            WHEN SEL5_S_INPUT <= SEL5_F_INPUT 
 -              AND SEL5_S_INPUT <= SEL5_T_INPUT
                    DISPLAY SEL5_S_OUTPUT " IS THE LOWEST NUMBER"
+                   MOVE SEL5_S_OUTPUT_FH TO SEL5_RESULT_FH
 
            WHEN OTHER 
                DISPLAY SEL5_T_OUTPUT " IS THE LOWEST NUMBER"
-           END-EVALUATE.                                      
+               MOVE SEL5_T_OUTPUT_FH TO SEL5_RESULT_FH
+           END-EVALUATE.   
+
+               WRITE SEL5_RECORD.
+           CLOSE SEL5-FILE.    
+
+           DISPLAY "VIEW HISTORY? (Y/y): " WITH NO ADVANCING.
+           ACCEPT SEL5_HISTORY.
+
+           IF SEL5_HISTORY = "Y" OR SEL5_HISTORY = "y"
+           MOVE "N" TO SEL5_EOF
+               OPEN INPUT SEL5-FILE
+               PERFORM UNTIL SEL5_EOF = "Y"
+                   READ SEL5-FILE
+                       AT END
+                           DISPLAY "END OF HISTORY"
+                           MOVE "Y" TO SEL5_EOF
+
+                       NOT AT END
+                           DISPLAY "==============================="
+                           DISPLAY "FIRST NUMBER: " SEL5_F_OUTPUT_FH
+                           DISPLAY "SECOND NUMBER: " SEL5_S_OUTPUT_FH
+                           DISPLAY "THIRD NUMBER: " SEL5_T_OUTPUT_FH
+                           DISPLAY "LOWEST NUMBER: " SEL5_RESULT_FH
+                           DISPLAY "==============================="
+                           DISPLAY SPACE
+
+                   END-READ
+               END-PERFORM
+               CLOSE SEL5-FILE
+           END-IF.                               
            
            STOP RUN.
