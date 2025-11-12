1e5248814fed3317d0f7118d76248fab7834b0a4
Sanchez <gabriel.raknchez@gmail.com>
2025-11-12 21:29:44 +0800

    selection 2 complete tests

--- selection/sel_2.cbl
+++ selection/sel_2.cbl
@@ -2,11 +2,26 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. POSITIVE-OR-NEGATIVE.
 
+       ENVIRONMENT DIVISION.
+           INPUT-OUTPUT SECTION. 
+               FILE-CONTROL.
+           SELECT SEL2-FILE ASSIGN TO "sel_2.dat"
+-              ORGANIZATION IS LINE SEQUENTIAL. 
+
        DATA DIVISION.
+       FILE SECTION.
+           FD SEL2-FILE.
+
+       01  SEL2_RECORD.
+            05  SEL2_INPUT_OUT_FH              PIC +Z(7).
+            05  SEL2_RESULT_FH                 PIC X(20).
+
        WORKING-STORAGE SECTION.
        01  SEL2_USER_INPUT             PIC S9(7).
        01  SEL2_INPUT_OUT              PIC Z(7).
-       01  SEL2_RESULT                 PIC X (20).
+       01  SEL2_RESULT                 PIC X(20).
+       01  SEL2_EOF                    PIC X VALUE "N".
+       01  SEL2_HISTORY                PIC X VALUE "N".
       
       
 
@@ -29,4 +44,35 @@
                DISPLAY SEL2_INPUT_OUT, SEL2_RESULT
                
            END-EVALUATE.
+
+           OPEN EXTEND SEL2-FILE.
+               MOVE SEL2_USER_INPUT TO SEL2_INPUT_OUT_FH.
+               MOVE SEL2_RESULT TO SEL2_RESULT_FH.
+
+               WRITE SEL2_RECORD.
+           CLOSE SEL2-FILE.
+
+           DISPLAY "VIEW HISTORY? (Y/y): " WITH NO ADVANCING.
+           ACCEPT SEL2_HISTORY.
+
+           IF SEL2_HISTORY = "Y" OR SEL2_HISTORY = "y"
+           MOVE "N" TO SEL2_EOF
+               OPEN INPUT SEL2-FILE
+               PERFORM UNTIL SEL2_EOF = "Y"
+                   READ SEL2-FILE
+                       AT END
+                           DISPLAY "END OF HISTORY"
+                           MOVE "Y" TO SEL2_EOF
+
+                       NOT AT END
+                           DISPLAY "==============================="
+                           DISPLAY "NUMBER: " SEL2_INPUT_OUT_FH
+                           DISPLAY "RESULT: " SEL2_RESULT_FH
+                           DISPLAY "==============================="
+                           DISPLAY SPACE
+
+                   END-READ
+               END-PERFORM
+               CLOSE SEL2-FILE
+           END-IF.
            STOP RUN.
